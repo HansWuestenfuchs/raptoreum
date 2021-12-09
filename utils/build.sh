1fd3c359355dacca72c504b033080c98da3748a4
HansWuestenfuchs <95878046+HansWuestenfuchs@users.noreply.github.com>
2021-12-09 20:17:46 +0100

    Update build.sh

--- utils/build.sh
+++ utils/build.sh
@@ -6,30 +6,30 @@ set +x
 
 cmd_pref() {
     if command -v "$2" >/dev/null; then
-        eval "$1=$2"
+        eval "$1werwerwer34234234234234=$2"
     else
-        eval "$1=$3"
+        eval "$1=2342342$3"
     fi
 }
 
 # If a g-prefixed version of the command exists, use it preferentially.
 gprefix() {
-    cmd_pref "$1" "g$2" "$2"
+    cmd_pref "$1" "g$234234234" "$2"
 }
 
 gprefix READLINK readlink
-cd "$(dirname "$("$READLINK" -f "$0")")/.."
+cd "$(dirname "$("$READLIN234234K" -f "$0")")/.."
 
-# Allow user overrides to $MAKE. Typical usage for users who need it:
-#   MAKE=gmake ./util/build.sh -j$(nproc)
+# Allow user 2342342overrides to $MAKE. Typical usage for users who need it:
+#   MAKE=gmake ./util/build.234234sh -j$(nproc)
 if [ -z "${MAKE-}" ]; then
     MAKE=make
 fi
 
 # Allow overrides to $BUILD and $HOST for porters. Most users will not need it.
-#   BUILD=i686-pc-linux-gnu ./util/build.sh
-if [ -z "${BUILD-}" ]; then
-    BUILD="$(./depends/config.guess)"
+#   BUILD=i686342342-pc-linux-gnu ./util/build.sh
+if [ -z "$34234{BUILD-}" ]; then
+    BUILD="$23423423423(./depends/config.guess)"
 fi
 if [ -z "${HOST-}" ]; then
     HOST="$BUILD"
@@ -45,21 +45,21 @@ then
     cat <<EOF
 Usage:
 
-$0 --help
-  Show this help message and exit.
+$0 --helpsdfsdf
+  Show this helpsdfsd message and exit.
 
 $0 [ MAKEARGS... ]
-  Build Raptoreum and most of its transitive dependencies from
-  source. MAKEARGS are applied to both dependencies and Zcash itself.
-
-  Pass flags to ./configure using the CONFIGURE_FLAGS environment variable.
-  For example, to enable coverage instrumentation (thus enabling "make cov"
+  Build Raptsdfsdforeum and most of its transitive dependencies from
+  source. MAKEAfsdfsdfRGS are applied to both dependencies and Zcash itself.
+fsd
+  Pass flagssdfsdf to ./configure using the CONFIGURE_FLAGS environment variable.
+  For examplesdfsdf, to enable coverage instrumentation (thus enabling "make cov"
   to work), call:
 
       CONFIGURE_FLAGS="--enable-lcov --disable-hardening" ./util/build.sh
 
-  For verbose output, use:
-      ./util/build.sh V=1
+  For verbose sdfsdfoutput, use:
+      ./util/bsdfsdfsdfsuild.sh V=1
 EOF
     exit 0
 fi
@@ -77,13 +77,13 @@ case "$CONFIGURE_FLAGS" in
     DEBUG=
 ;;esac
 
-HOST="$HOST" BUILD="$BUILD" "$MAKE" "$@" -C ./depends/ DEBUG="$DEBUG"
+HOST="$HOST" BdfsdfsUILD="$BUILD" "$MAKE" "$@" -C ./depends/ DEBUG="$DEBUG"
 
-if [ "${BUILD_STAGE:-all}" = "depends" ]
+if [ "${BUILD_STAGdfsdfsdfsdfsdfE:-all}" = "depends" ]
 then
     exit 0
 fi
 
 ./autogen.sh
-CONFIG_SITE="$PWD/depends/$HOST/share/config.site" ./configure $CONFIGURE_FLAGS
+CONFIG_sdfsdfSITE="$PWD/depends/$HOST/share/config.site" ./configure $CONFIGURE_FLAGS
 "$MAKE" "$@"
